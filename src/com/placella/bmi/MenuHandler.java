5ebf9cc0e136fac945033eed938a559d9343b62f
Rouslan Placella <rouslan@placella.com>
2012-10-15 01:19:33 +0100

    Added about dialog

--- src/com/placella/bmi/MenuHandler.java
+++ src/com/placella/bmi/MenuHandler.java
@@ -1,6 +1,7 @@
 package com.placella.bmi;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.*;
 import android.net.Uri;
 import android.view.MenuItem;
@@ -8,17 +9,26 @@ import android.widget.Toast;
 
 public class MenuHandler {
     public static void onOptionsItemSelected(MenuItem item, Activity self) {
-		String url = "https://github.com/roccivic/android-bmi-app";
-		Intent i = new Intent(Intent.ACTION_VIEW);
-		Uri u = Uri.parse(url);
-		i.setData(u);
-		try {
-			// Start the activity
-			self.startActivity(i);
-		} catch (ActivityNotFoundException e) {
-			// Raise on activity not found
-			Toast message = Toast.makeText(self, "Browser not found.", Toast.LENGTH_SHORT);
-			message.show();
-		}
+        if (item.getItemId() == R.id.source_code) {
+			String url = "https://github.com/roccivic/android-bmi-app";
+			Intent i = new Intent(Intent.ACTION_VIEW);
+			Uri u = Uri.parse(url);
+			i.setData(u);
+			try {
+				// Start the activity
+				self.startActivity(i);
+			} catch (ActivityNotFoundException e) {
+				// Raise on activity not found
+				Toast message = Toast.makeText(self, "Browser not found.", Toast.LENGTH_SHORT);
+				message.show();
+			}
+        } else if (item.getItemId() == R.id.about) {
+        	new AlertDialog
+	    	    .Builder(self)
+		    	.setMessage("BMI calculator\nversion 1.0\nOctober 2012")
+		    	.setPositiveButton(R.string.ok, null)
+		    	.create()
+		    	.show();
+        }
     }
 }
